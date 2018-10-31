c98989d8e45258ba4e24e4e8c17fea58cb79b98d
Suresh Rajupalem <rs.babu81@gmail.com>
2018-10-31 04:12:01 +0200

    Add project files.

---
+++ WordCountApp/WordCount.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WordCountApp
+{
+    public partial class WordCount : System.Web.UI.Page
+    {
+         const string inputPath = @"C:\temp\wordlist.txt";
+         const string ouputPath = @"C:\temp\results.txt";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var wordCounters = new IWordCounter[]{ };
+            WordCounter wcr = new WordCounter();
+          
+            foreach (var wc in wordCounters)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();               
+                var wcResult = wcr.CountWords(inputPath);
+                using (StreamWriter writer = new StreamWriter(ouputPath))
+                {
+                    writer.WriteLine(wcResult.GetType().Name + "   " + Convert.ToString(wcResult.Count) + Environment.NewLine);
+                }
+              
+             
+            }
+
+         
+        }
+    }
+}
---
+++ WordCountApp/WordCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+
+namespace WordCountApp
+{
+    public class WordCounter: IWordCounter
+    {
+        private static readonly char[] separators = { ' ' };
+        
+        public IDictionary<string, int> CountWords(string path)
+        {
+            var wordCount = new Dictionary<string, int>();
+
+            foreach (var line in File.ReadLines(path, Encoding.UTF8))
+            {
+                var words = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    int count;
+                    wordCount.TryGetValue(word, out count);
+                    wordCount[word] = count + 1;
+                }
+            }
+
+            return wordCount;
+        }
+   
+
+    }
+}
